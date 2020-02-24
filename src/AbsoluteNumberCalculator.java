6bbbf001ef377337b298434b61d8477aa2e4baed
LongDo612 <36716887+LongDo612@users.noreply.github.com>
2020-02-24 17:19:19 +0700

    Update AbsoluteNumberCalculator.java

--- src/AbsoluteNumberCalculator.java
+++ src/AbsoluteNumberCalculator.java
@@ -1,8 +1,5 @@
 public class AbsoluteNumberCalculator {
     public static int findAbsolute(int number){
-        if (number>Integer.MAX_VALUE || number<Integer.MIN_VALUE){
-            throw new RuntimeException("out of range exception");
-        }
         return Math.abs(number);
     }
 }
